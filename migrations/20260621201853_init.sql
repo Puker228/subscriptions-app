6f7811f537c3141f518ab94cf872a3749ada32fa
puker <puker@puker.com>
2026-06-22 00:34:21 +0300

    first migration

---
+++ migrations/20260621201853_init.sql
@@ -0,0 +1,13 @@
+-- +goose Up
+CREATE TABLE IF NOT EXISTS subscriptions (
+    id UUID PRIMARY KEY,
+    service_name TEXT NOT NULL,
+    price INTEGER NOT NULL,
+    user_id UUID NOT NULL,
+
+    start_date TIMESTAMPTZ NOT NULL,
+    end_date DATE NULL
+);
+
+-- +goose Down
+DROP TABLE subscriptions;
